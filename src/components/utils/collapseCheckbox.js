725c2c8a7c1654c816a57c023ac6b5d6575c79f6
Donald Njumbe Ediage <ediage@yahoo.com>
2019-08-10 17:07:17 +0300

    building the filters

--- src/components/utils/collapseCheckbox.js
+++ src/components/utils/collapseCheckbox.js
@@ -37,8 +37,36 @@ class CollapseCheckbox extends Component {
 
   renderList = () =>
     this.props.list
-      ? this.props.list.map((value, i) => <div key={i}>hey</div>)
+      ? this.props.list.map(value => (
+          <ListItem key={value._id} style={{ padding: "10px 0" }}>
+            <ListItemText primary={value.name} />
+            <ListItemSecondaryAction>
+              <Checkbox
+                color="primary"
+                onChange={this.handleToggle(value._id)}
+                checked={this.state.checked.indexOf(value._id) !== -1}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))
       : null;
+
+  handleToggle = value => () => {
+    const { checked } = this.state;
+    // This is the same as above 'const checked = this.state.checked;'
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+    // this.props.handleFilters(newChecked) or as a call back function
+    this.setState({ checked: newChecked }, () => {
+      this.props.handleFilters(newChecked);
+    });
+  };
 
   render() {
     return (
